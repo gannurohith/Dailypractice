e56df2ed6b43e8607d4e8782d5963cea15775421
gannurohith <49453539+gannurohith@users.noreply.github.com>
2021-04-23 03:58:35 +0530

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def second_smallest(numbers):
+  if (len(numbers)<2):
+    return
+  if ((len(numbers)==2)  and (numbers[0] == numbers[1]) ):
+    return
+  dup_items = set()
+  uniq_items = []
+  for x in numbers:
+    if x not in dup_items:
+      uniq_items.append(x)
+      dup_items.add(x)
+  uniq_items.sort()    
+  return  uniq_items[1]   
+
+print(second_smallest([1, 2, -8, -2, 0, -2]))
+print(second_smallest([1, 1, 0, 0, 2, -2, -2]))
+print(second_smallest([1, 1, 1, 0, 0, 0, 2, -2, -2]))
+print(second_smallest([2,2]))
+print(second_smallest([2]))
+
+
+# In[2]:
+
+
+from itertools import zip_longest, chain, tee
+def replace2copy(lst):
+    lst1, lst2 = tee(iter(lst), 2)
+    return list(chain.from_iterable(zip_longest(lst[1::2], lst[::2])))
+n = [0,1,2,3,4,5]
+print(replace2copy(n))
+
+
+# In[ ]:
+
+
+
+
